7dd449df8c73bfd43ced0436031e51f98059d454
pawszu908 <pawszu@student.agh.edu.pl>
2026-03-21 14:53:12 +0100

    poprawka xd

--- RSript.R
+++ RSript.R
@@ -3,6 +3,7 @@ library(tidyverse)
 HSI <- read.csv("data/HSI.csv")
 N225 <-read.csv("data/N225.csv")
 
-HSI <- inner_join(HSI, N225, by = "date")
+HSI <- HSI[HSI$date %in% N225$date, ]
+N225 <- N225[N225$date %in% HSI$date, ]
 
-N225 <- inner_join(N255, HSI, by = "date")
+
